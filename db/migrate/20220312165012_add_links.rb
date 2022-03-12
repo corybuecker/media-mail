3b5bc10adf6d2c3132023ce02a7b0682dd3ce525
Cory Buecker <email@corybuecker.com>
2022-03-12 10:58:28 -0600

    initial commit

---
+++ db/migrate/20220312165012_add_links.rb
@@ -0,0 +1,11 @@
+class AddLinks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :links do |t|
+      t.string :url, null: false
+
+      t.timestamps
+    end
+
+    add_index :links, :url
+  end
+end
