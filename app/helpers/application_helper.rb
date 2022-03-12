57f91c1712b1ef2b36951512181db48023cf207a
Cory Buecker <email@corybuecker.com>
2022-03-12 11:05:14 -0600

    Apply rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# main app helper
 module ApplicationHelper
 end
--- db/migrate/20220312165012_add_links.rb
+++ db/migrate/20220312165012_add_links.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# migration
 class AddLinks < ActiveRecord::Migration[7.0]
   def change
     create_table :links do |t|
